a4b639a3138e43787e1e0da62a9408600406aab1
Adrian Orłów <adrian@orlow.me>
2026-01-10 04:47:35 +0100

    fix: report comment component not visible correctly on xs devices

--- src/components/report/ReportQuestionAnswers/Comment/CommentView.tsx
+++ src/components/report/ReportQuestionAnswers/Comment/CommentView.tsx
@@ -11,14 +11,14 @@ interface Props {
 const Comment = ({ comment, experts }: Props): JSX.Element => {
   const expert = useMemo(
     () => experts.find((expert) => expert.id === comment.expertId),
-    [experts, comment],
+    [experts, comment.expertId],
   );
 
   return (
     <div className="bg-gray p-6 rounded-3xl flex flex-col gap-4">
       <div className="whitespace-pre-line">{comment.value}</div>
       {expert && (
-        <div className="flex justify-between gap-4 items-center">
+        <div className="flex justify-between gap-4 flex-col sm:flex-row sm:items-center">
           <div className="flex gap-3 items-center">
             <div className="flex rounded-full object-cover border border-dark-gray overflow-hidden flex-shrink-0 h-[48px] w-[48px]">
               <Image
@@ -36,7 +36,7 @@ const Comment = ({ comment, experts }: Props): JSX.Element => {
             <div className="font-bold">{expert.person.name}</div>
           </div>
           <div>
-            <Link href={expert.organisation.imageUrl}>
+            <Link href={expert.organisation.websiteUrl || ""} target="_blank">
               {/* disabled due to non-deterministic sizings */}
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
