bacac5b4bdd33e8b9006859c685306abde377b94
Mikolaj Glistak <mikolaj.glistak@gmail.com>
2026-01-12 20:30:06 +0100

    Proper config

--- src/components/layout/AppLayout/AppLayoutView.tsx
+++ src/components/layout/AppLayout/AppLayoutView.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 import { Roboto } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const roboto = Roboto({
   subsets: ["latin", "latin-ext"],
@@ -14,15 +15,7 @@ const roboto = Roboto({
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl" className={roboto.className}>
-      <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-T765V9Z5');</script>
-        <!-- End Google Tag Manager -->
-      </head>
+      <GoogleTagManager gtmId="GTM-T765V9Z5" />
       <body className="p-0">
         <div className="flex flex-col gap-4 w-full justify-center md:gap-6 min-h-screen">
           <div className="flex flex-col gap-4 w-full mx-auto">
@@ -33,10 +26,6 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
             <Footer />
           </div>
         </div>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T765V9Z5"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
       </body>
     </html>
   );
